8708ccf7ec98147cb2c111ad56174a3d704330c3
Zuzia <zuzia.zur@gmail.com>
2023-09-26 12:34:17 +0200

    gradient

--- main.js
+++ main.js
@@ -9,4 +9,24 @@ console.log (`Nazywam się ${firstName} i mam ${age} lat`);
 
 const header = document.querySelector(".page-header__heading--js")
 
+function calculate(myNumber) {
+    return myNumber*7;
+}
+
+console.log(calculate(2))
+
+const deathStar = {
+    diameter: 120000,
+    fire: (target) => {
+        console.log (`${target} destroyed`)
+    },
+    isOperating: true,
+    levels: 337,
+    name: 'Death Star',
+    population: 1000
+}
+
+console.log(deathStar)
+deathStar.fire('bumbum')
+
 console.log(header)
